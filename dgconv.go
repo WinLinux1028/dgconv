22fe1c78784dc4e8bebef324f72e386d543cee73
WinLinux1028 <yjsnpi@main>
2020-11-14 14:33:19 +0900

    aaaa

--- dgconv.go
+++ dgconv.go
@@ -63,7 +63,7 @@ func Getuser(s *discordgo.Session, user string) (id string) {
 	return
 }
 
-//Getrole returns user's ID from ID, mention, username, and nickname.
+//Getrole returns role's id from ID, mention, name.
 func Getrole(s *discordgo.Session, m *discordgo.MessageCreate, role string) (id string) {
 	switch {
 	case regexp.MustCompile(`[0-9]+`).MatchString(role):
@@ -87,6 +87,41 @@ func Getrole(s *discordgo.Session, m *discordgo.MessageCreate, role string) (id 
 		for _, rol := range g.Roles {
 			if role == rol.Name {
 				id = rol.ID
+				break
+			}
+		}
+	}
+	return
+}
+
+//Getchannel returns channel's if from ID, mantion, name.
+func Getchannel(s *discordgo.Session, channel string) (id string) {
+	switch {
+	case regexp.MustCompile(`[0-9]+`).MatchString(channel):
+		_, check := s.State.Channel(channel)
+		if check != nil {
+		} else {
+			id = channel
+			break
+		}
+		fallthrough
+	case regexp.MustCompile(`<#[0-9]+>`).MatchString(channel):
+		_, check := s.User(channel[2 : len(channel)-1])
+		if check != nil {
+		} else {
+			id = channel[2 : len(channel)-1]
+			break
+		}
+		fallthrough
+	default:
+		for _, guild := range s.State.Guilds {
+			for _, ch := range guild.Channels {
+				if channel == ch.Name {
+					id = ch.ID
+					break
+				}
+			}
+			if len(id) != 0 {
 				break
 			}
 		}
